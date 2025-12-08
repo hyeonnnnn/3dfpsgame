2731adb09176399d6529431b613e95abf17a92a4
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2025-12-08 16:54:07 +0900

    feat: 플레이어 점프

--- 3dfpsgame/Assets/02. Scripts/Player/PlayerMove.cs
+++ 3dfpsgame/Assets/02. Scripts/Player/PlayerMove.cs
@@ -1,8 +1,19 @@
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMove : MonoBehaviour
 {
-    [SerializeField] private float _speed = 1f;
+    [SerializeField] private float _moveSpeed = 1f;
+    [SerializeField] private float _jumpForce = 5f;
+    private const float Gravity = -9.81f;
+
+    private CharacterController _characterController;
+    private float _yVelocity = 0f;
+
+    private void Awake()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
 
     private void Update()
     {
@@ -11,14 +22,22 @@ public class PlayerMove : MonoBehaviour
 
     private void Move()
     {
-        float moveX = Input.GetAxis("Horizontal") * _speed * Time.deltaTime;
-        float moveZ = Input.GetAxis("Vertical") * _speed * Time.deltaTime;
+        _yVelocity += Gravity * Time.deltaTime;
+
+        float moveX = Input.GetAxis("Horizontal") * _moveSpeed * Time.deltaTime;
+        float moveZ = Input.GetAxis("Vertical") * _moveSpeed * Time.deltaTime;
+
+        if (Input.GetButtonDown("Jump") && _characterController.isGrounded)
+        {
+            _yVelocity = _jumpForce;
+        }
 
         Vector3 direction = new Vector3(moveX, 0, moveZ);
         direction.Normalize();
 
         direction = Camera.main.transform.TransformDirection(direction);
+        direction.y = _yVelocity;
 
-        transform.position += direction * _speed * Time.deltaTime;
+        _characterController.Move(direction * _moveSpeed * Time.deltaTime);
     }
 }
