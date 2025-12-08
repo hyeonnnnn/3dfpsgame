3044ad4b7577aa387d108122a97e3b441e4359b8
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2025-12-08 14:31:20 +0900

    chore: 에러 테스트

---
+++ 3dfpsgame/Assets/02. Scripts/ErrorTest2.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ErrorTest2 : MonoBehaviour
+{
+    private void Start()
+    {
+        // MissingReferenceException
+
+        // IndexOutOfRangeException
+
+        // DevideByZeroException
+    }
+}
