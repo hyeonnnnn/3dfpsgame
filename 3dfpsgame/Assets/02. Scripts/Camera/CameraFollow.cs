e3328bbe22e892747706493fda96d5bb97547ae1
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2025-12-09 10:15:53 +0900

    feat: fps/tps 카메라 전환

--- 3dfpsgame/Assets/02. Scripts/Camera/CameraFollow.cs
+++ 3dfpsgame/Assets/02. Scripts/Camera/CameraFollow.cs
@@ -1,11 +1,38 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    [SerializeField] private Transform _target;
+    [SerializeField] private Transform _fpsViewTarget;
+    [SerializeField] private Transform _tpsViewTarget;
+    [SerializeField] private float _smoothTime = 0.3f;
+
+    private bool _canSwitch = false;
 
     private void LateUpdate()
     {
-        transform.position = _target.position;
-}
+        TrySwitchView();
+    }
+
+    private void TrySwitchView()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            _canSwitch = !_canSwitch;
+        }
+
+        if (_canSwitch == false)
+        {
+            SwitchView(_fpsViewTarget);
+        }
+        else
+        {
+            SwitchView(_tpsViewTarget);
+        }
+    }
+
+    private void SwitchView(Transform target)
+    {
+        transform.DOMove(target.position, _smoothTime);
+    }
 }
