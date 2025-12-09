f84090dfda22a9314475fbe6c294a84dcce51099
huiyeon <128331103+hyeonnnnn@users.noreply.github.com>
2025-12-09 16:10:05 +0900

    feat: 폭탄 UI

--- Assets/02. Scripts/Player/PlayerFire.cs
+++ Assets/02. Scripts/Player/PlayerFire.cs
@@ -1,25 +1,51 @@
+using System;
 using UnityEngine;
 
 public class PlayerFire : MonoBehaviour
 {
     [SerializeField] private Transform _fireTransform;
+
+    [Header("폭탄")]
     [SerializeField] private Bomb _bombPrefab;
-    [SerializeField] private float ThrowPower = 15f;
+    [SerializeField] private int _bombMaxcount = 5;
+    [SerializeField] private int _bombCount;
+
+    [SerializeField] private float _throwPower = 15f;
     private Transform _cameraTransform;
+
+    public event Action<int> OnBombCountChanged;
 
     private void Awake()
     {
         _cameraTransform = Camera.main.transform;
     }
 
+    private void Start()
+    {
+        _bombCount = _bombMaxcount;
+    }
+
     private void Update()
+    {
+        TryFire();
+    }
+
+    private void TryFire()
     {
         if (Input.GetMouseButtonDown(1))
         {
-            Bomb bomb = Instantiate(_bombPrefab, _fireTransform.position, Quaternion.identity);
-            Rigidbody rigidbody = bomb.GetComponent<Rigidbody>();
-
-            rigidbody.AddForce(_cameraTransform.forward * ThrowPower, ForceMode.VelocityChange);
+            if (_bombCount <= 0) return;
+            Fire();
+            _bombCount--;
+            OnBombCountChanged?.Invoke(_bombCount);
         }
+    }
+
+    private void Fire()
+    {
+        Bomb bomb = Instantiate(_bombPrefab, _fireTransform.position, Quaternion.identity);
+        Rigidbody rigidbody = bomb.GetComponent<Rigidbody>();
+
+        rigidbody.AddForce(_cameraTransform.forward * _throwPower, ForceMode.VelocityChange);
     }
 }
